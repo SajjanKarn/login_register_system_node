3a9950c26f2ceb8caa49b8ab1e5096d8ee24f664
ManishKarn56 <69583236+ManishKarn56@users.noreply.github.com>
2021-10-12 21:48:50 +0545

    Update app.js

--- app.js
+++ app.js
@@ -41,27 +41,18 @@ app
     const { email, password } = req.body;
 
     // check for missing filds
-    if (!email || !password) {
-      res.send("Please enter all the fields");
-      return;
-    }
+    if (!email || !password) return res.send("Please enter all the fields");
 
     const doesUserExits = await User.findOne({ email });
 
-    if (!doesUserExits) {
-      res.send("invalid username or password");
-      return;
-    }
+    if (!doesUserExits) return res.send("invalid username or password");
 
     const doesPasswordMatch = await bcrypt.compare(
       password,
       doesUserExits.password
     );
 
-    if (!doesPasswordMatch) {
-      res.send("invalid useranme or password");
-      return;
-    }
+    if (!doesPasswordMatch) return res.send("invalid useranme or password");
 
     // else he\s logged in
     req.session.user = {
@@ -74,17 +65,11 @@ app
     const { email, password } = req.body;
 
     // check for missing filds
-    if (!email || !password) {
-      res.send("Please enter all the fields");
-      return;
-    }
+    if (!email || !password) return res.send("Please enter all the fields");
 
     const doesUserExitsAlreay = await User.findOne({ email });
 
-    if (doesUserExitsAlreay) {
-      res.send("A user with that email already exits please try another one!");
-      return;
-    }
+    if (doesUserExitsAlreay) return res.send("A user with that email already exits please try another one!");
 
     // lets hash the password
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -94,7 +79,7 @@ app
       .save()
       .then(() => {
         res.send("registered account!");
-        return;
+        res.redirect("/login");
       })
       .catch((err) => console.log(err));
   });
